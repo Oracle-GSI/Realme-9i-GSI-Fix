3cb4b8ccac3a675961b936558e49836dc98c35bc
Oracle-GSI <121375771+Oracle-GSI@users.noreply.github.com>
2022-12-27 13:10:07 +0300

    Create service.sh

---
+++ common/service.sh
@@ -0,0 +1,57 @@
+#!/system/bin/sh
+# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
+# This will make your scripts compatible even if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+# This script will be executed in late_start service mode
+# More info in the main Magisk thread
+
+echo 1 > /sys/class/power_supply/usb/otg_switch
+
+sleep 30
+
+echo 1 > /sys/class/power_supply/usb/otg_switch
+
+sleep 30
+
+echo 1 > /sys/class/power_supply/usb/otg_switch
+
+sleep 30
+
+echo 1 > /sys/class/power_supply/usb/otg_switch
+
+sleep 30
+
+echo 1 > /sys/class/power_supply/usb/otg_switch
+
+sleep 30
+
+echo 1 > /sys/class/power_supply/usb/otg_switch
+
+sleep 30
+
+echo 1 > /sys/class/power_supply/usb/otg_switch
+
+sleep 30
+
+echo 1 > /sys/class/power_supply/usb/otg_switch
+
+sleep 30
+
+echo 1 > /sys/class/power_supply/usb/otg_switch
+
+sleep 30
+
+echo 1 > /sys/class/power_supply/usb/otg_switch
+
+sleep 30
+
+echo 1 > /sys/class/power_supply/usb/otg_switch
+
+sleep 30
+
+echo 1 > /sys/class/power_supply/usb/otg_switch
+
+sleep 30
+
+echo 1 > /sys/class/power_supply/usb/otg_switch
